acd597ed6aaaef444b1180f60337e20fc461272e
Ralph Boadi <147431299+ralphboadi@users.noreply.github.com>
2025-06-27 10:44:10 -0600

    customer_metrics_query.sql

--- SQL CODE/SQLQuery/customer_metrics_query.sql
+++ SQL CODE/SQLQuery/customer_metrics_query.sql
@@ -5,8 +5,8 @@ SELECT
     Customer_Name,
     Segment,
     Region,
-    SUM(Sales) AS Total_Sales,
-    SUM(Profit) AS Total_Profit,
+    ROUND(SUM(Sales), 2) AS Total_Sales,
+    ROUND(SUM(Profit), 2) AS Total_Profit,
     COUNT(DISTINCT Order_ID) AS Order_Count
 FROM [Sample - Superstore]
 GROUP BY Customer_ID, Customer_Name, Segment, Region;
