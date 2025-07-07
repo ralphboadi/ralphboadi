8b5ea33cc1ccc377352fd1024ac0596fd2ea1906
Ralph Boadi <147431299+ralphboadi@users.noreply.github.com>
2025-07-07 12:05:15 -0600

    Delete SQL CODE/SQLQuery directory

--- SQL CODE/SQLQuery/total_sales_profit_quantity.sql
+++
@@ -1,7 +0,0 @@
-SELECT 
-    SUM(Sales) AS total_sales,
-    SUM(Profit) AS total_profit,
-    SUM(Quantity) AS total_quantity,
-    ROUND(SUM(Profit) / NULLIF(SUM(Sales), 0), 2) AS profit_margin
-FROM 
-[Sample - Superstore];
